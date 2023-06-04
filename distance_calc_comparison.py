da67a2684581dbc640d1d728775f2ce9ced580a9
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-06-04 15:04:56 -0300

    feat: change vectors to higher dimensions

--- distance_calc_comparison.py
+++ distance_calc_comparison.py
@@ -9,25 +9,25 @@ def compare_vector_distances():
     v2_low = np.random.rand(2)
     v3_low = np.random.rand(2)
 
-    v1_high = np.random.rand(30)
-    v2_high = np.random.rand(30)
-    v3_high = np.random.rand(30)
+    v1_high = np.random.rand(500)
+    v2_high = np.random.rand(500)
+    v3_high = np.random.rand(500)
 
     # Calculate Euclidean distances and cosine similarities
     print("DISTANCE BETWEEN VECTORS IN LOW DIMENSIONAL SPACE")
     print("Euclidean distance between v1_low and v2_low: ", euclidean_distance(v1_low, v2_low))
     print("Euclidean distance between v1_low and v3_low: ", euclidean_distance(v1_low, v3_low))
 
-    print("Cosine similarity between v1_low and v2_low: ", cosine_similarity(v1_low, v2_low))
-    print("Cosine similarity between v1_low and v3_low: ", cosine_similarity(v1_low, v3_low))
+    print("Cosine similarity between v1_low and v2_low: ", 1 - cosine_similarity(v1_low, v2_low))
+    print("Cosine similarity between v1_low and v3_low: ", 1 - cosine_similarity(v1_low, v3_low))
     print("\n\n")
 
     print("DISTANCE BETWEEN VECTORS IN HIGH DIMENSIONAL SPACE")
     print("Euclidean distance between v1_high and v2_high: ", euclidean_distance(v1_high, v2_high))
     print("Euclidean distance between v1_high and v3_high: ", euclidean_distance(v1_high, v3_high))
 
-    print("Cosine similarity between v1_high and v2_high: ", cosine_similarity(v1_high, v2_high))
-    print("Cosine similarity between v1_high and v3_high: ", cosine_similarity(v1_high, v3_high))
+    print("Cosine similarity between v1_high and v2_high: ", 1 - cosine_similarity(v1_high, v2_high))
+    print("Cosine similarity between v1_high and v3_high: ", 1 - cosine_similarity(v1_high, v3_high))
     print("\n\n")
 
 compare_vector_distances()
