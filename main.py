f5855b9fcc6bf20ba65f366e66abdcfe5145e2ec
josethz00 <jose.soares18@fatec.sp.gov.br>
2023-06-04 14:51:43 -0300

    feat: cosine similarity

--- main.py
+++ main.py
@@ -3,28 +3,45 @@ import numpy as np
 
 # Your vectors
 np.random.seed(0)  # for reproducibility
-v1 = np.random.rand(10)
-v2 = np.random.rand(10)
-v3 = np.random.rand(10)
+v1_low = np.random.rand(2)
+v2_low = np.random.rand(2)
+v3_low = np.random.rand(2)
 
-# Calculate Euclidean distances
-euclidean_distance12 = np.linalg.norm(v1 - v2)
-euclidean_distance13 = np.linalg.norm(v1 - v3)
+v1_high = np.random.rand(30)
+v2_high = np.random.rand(30)
+v3_high = np.random.rand(30)
 
-# Calculate cosine similarities
-cosine_similarity12 = np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
-cosine_similarity13 = np.dot(v1, v3) / (np.linalg.norm(v1) * np.linalg.norm(v3))
+# Calculate Euclidean distances and cosine similarities
+def calculate_metrics(v1, v2, v3):
+    euclidean_distance12 = np.linalg.norm(v1 - v2)
+    euclidean_distance13 = np.linalg.norm(v1 - v3)
+    cosine_similarity12 = np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
+    cosine_similarity13 = np.dot(v1, v3) / (np.linalg.norm(v1) * np.linalg.norm(v3))
+    return [euclidean_distance12, euclidean_distance13], [cosine_similarity12, cosine_similarity13]
 
-# Plot Euclidean distances
+euclidean_low, cosine_low = calculate_metrics(v1_low, v2_low, v3_low)
+euclidean_high, cosine_high = calculate_metrics(v1_high, v2_high, v3_high)
+
+# Plot Euclidean distances vs Cosine similarities
 plt.figure(figsize=(10, 5))
-plt.subplot(1, 2, 1)
-plt.bar(['v1-v2', 'v1-v3'], [euclidean_distance12, euclidean_distance13])
-plt.title('Euclidean Distance')
 
-# Plot cosine similarities
+plt.subplot(1, 2, 1)
+plt.scatter(euclidean_low, cosine_low)
+plt.xlabel('Euclidean Distance')
+plt.ylabel('Cosine Similarity')
+plt.title('Low-Dimensional Vectors')
+for i, txt in enumerate(['v1-v2', 'v1-v3']):
+    plt.annotate(txt, (euclidean_low[i], cosine_low[i]))
+plt.grid(True)
+
 plt.subplot(1, 2, 2)
-plt.bar(['v1-v2', 'v1-v3'], [cosine_similarity12, cosine_similarity13])
-plt.title('Cosine Similarity')
+plt.scatter(euclidean_high, cosine_high)
+plt.xlabel('Euclidean Distance')
+plt.ylabel('Cosine Similarity')
+plt.title('High-Dimensional Vectors')
+for i, txt in enumerate(['v1-v2', 'v1-v3']):
+    plt.annotate(txt, (euclidean_high[i], cosine_high[i]))
+plt.grid(True)
 
 plt.tight_layout()
 plt.show()
